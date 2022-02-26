8a989646ccb79bc8523764341b75620a35bda285
Prasad64 <36736034+Prasad64@users.noreply.github.com>
2022-02-27 00:57:46 +0530

    MAjor changes

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ console.log("Welcome");
 
 //Initialize Variables
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverpPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverpPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverpPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverpPath: "covers/1.jpg"},
-    {songName: "Salam-e-Ishq", filePath: "song/1.mp3", coverpPath: "covers/1.jpg"},
+    {songName: "Jashan-E-Bahara", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Salam-e-Ishq", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
 ]
+
+songItems.forEach((element, i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+})
 
 //audioElement.play();
 
@@ -20,11 +27,45 @@ let songs = [
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        
     }
 })
 
 //Listen to Events
-myProgressBar.addEventListener('timeupdate', ()=>{
-    console.log('timeupdate');
+audioElement.addEventListener('timeupdate', ()=>{
     // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+})
+
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        index = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${index+1}.mp3`;
+        audioElement.currentTime = 0;
+        audioElement.play();
+    })
 })
